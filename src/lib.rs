b308e11dd74624afbbaf79a2407cc4295b7b743f
XXIV <13811862+thechampagne@users.noreply.github.com>
2022-08-13 10:44:44 +0300

    Fix: made the functions public

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::io::Write;
 
-fn get_char(str: &str) -> Result<char, Box<dyn std::error::Error>> {
+pub fn get_char(str: &str) -> Result<char, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -9,7 +9,7 @@ fn get_char(str: &str) -> Result<char, Box<dyn std::error::Error>> {
     Ok(string.parse::<char>()?)
 }
 
-fn get_bool(str: &str) -> Result<bool, Box<dyn std::error::Error>> {
+pub fn get_bool(str: &str) -> Result<bool, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -18,7 +18,7 @@ fn get_bool(str: &str) -> Result<bool, Box<dyn std::error::Error>> {
     Ok(string.parse::<bool>()?)
 }
 
-fn get_i8(str: &str) -> Result<i8, Box<dyn std::error::Error>> {
+pub fn get_i8(str: &str) -> Result<i8, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -27,7 +27,7 @@ fn get_i8(str: &str) -> Result<i8, Box<dyn std::error::Error>> {
     Ok(string.parse::<i8>()?)
 }
 
-fn get_i16(str: &str) -> Result<i16, Box<dyn std::error::Error>> {
+pub fn get_i16(str: &str) -> Result<i16, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -36,7 +36,7 @@ fn get_i16(str: &str) -> Result<i16, Box<dyn std::error::Error>> {
     Ok(string.parse::<i16>()?)
 }
 
-fn get_i32(str: &str) -> Result<i32, Box<dyn std::error::Error>> {
+pub fn get_i32(str: &str) -> Result<i32, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -45,7 +45,7 @@ fn get_i32(str: &str) -> Result<i32, Box<dyn std::error::Error>> {
     Ok(string.parse::<i32>()?)
 }
 
-fn get_i64(str: &str) -> Result<i64, Box<dyn std::error::Error>> {
+pub fn get_i64(str: &str) -> Result<i64, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -54,7 +54,7 @@ fn get_i64(str: &str) -> Result<i64, Box<dyn std::error::Error>> {
     Ok(string.parse::<i64>()?)
 }
 
-fn get_i128(str: &str) -> Result<i128, Box<dyn std::error::Error>> {
+pub fn get_i128(str: &str) -> Result<i128, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -63,7 +63,7 @@ fn get_i128(str: &str) -> Result<i128, Box<dyn std::error::Error>> {
     Ok(string.parse::<i128>()?)
 }
 
-fn get_u8(str: &str) -> Result<u8, Box<dyn std::error::Error>> {
+pub fn get_u8(str: &str) -> Result<u8, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -72,7 +72,7 @@ fn get_u8(str: &str) -> Result<u8, Box<dyn std::error::Error>> {
     Ok(string.parse::<u8>()?)
 }
 
-fn get_u16(str: &str) -> Result<u16, Box<dyn std::error::Error>> {
+pub fn get_u16(str: &str) -> Result<u16, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -81,7 +81,7 @@ fn get_u16(str: &str) -> Result<u16, Box<dyn std::error::Error>> {
     Ok(string.parse::<u16>()?)
 }
 
-fn get_u32(str: &str) -> Result<u32, Box<dyn std::error::Error>> {
+pub fn get_u32(str: &str) -> Result<u32, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -90,7 +90,7 @@ fn get_u32(str: &str) -> Result<u32, Box<dyn std::error::Error>> {
     Ok(string.parse::<u32>()?)
 }
 
-fn get_u64(str: &str) -> Result<u64, Box<dyn std::error::Error>> {
+pub fn get_u64(str: &str) -> Result<u64, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -99,7 +99,7 @@ fn get_u64(str: &str) -> Result<u64, Box<dyn std::error::Error>> {
     Ok(string.parse::<u64>()?)
 }
 
-fn get_u128(str: &str) -> Result<u128, Box<dyn std::error::Error>> {
+pub fn get_u128(str: &str) -> Result<u128, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -108,7 +108,7 @@ fn get_u128(str: &str) -> Result<u128, Box<dyn std::error::Error>> {
     Ok(string.parse::<u128>()?)
 }
 
-fn get_f32(str: &str) -> Result<f32, Box<dyn std::error::Error>> {
+pub fn get_f32(str: &str) -> Result<f32, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -117,7 +117,7 @@ fn get_f32(str: &str) -> Result<f32, Box<dyn std::error::Error>> {
     Ok(string.parse::<f32>()?)
 }
 
-fn get_f64(str: &str) -> Result<f64, Box<dyn std::error::Error>> {
+pub fn get_f64(str: &str) -> Result<f64, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -126,7 +126,7 @@ fn get_f64(str: &str) -> Result<f64, Box<dyn std::error::Error>> {
     Ok(string.parse::<f64>()?)
 }
 
-fn get_isize(str: &str) -> Result<isize, Box<dyn std::error::Error>> {
+pub fn get_isize(str: &str) -> Result<isize, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -135,7 +135,7 @@ fn get_isize(str: &str) -> Result<isize, Box<dyn std::error::Error>> {
     Ok(string.parse::<isize>()?)
 }
 
-fn get_usize(str: &str) -> Result<usize, Box<dyn std::error::Error>> {
+pub fn get_usize(str: &str) -> Result<usize, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
@@ -144,7 +144,7 @@ fn get_usize(str: &str) -> Result<usize, Box<dyn std::error::Error>> {
     Ok(string.parse::<usize>()?)
 }
 
-fn get_string(str: &str) -> Result<String, Box<dyn std::error::Error>> {
+pub fn get_string(str: &str) -> Result<String, Box<dyn std::error::Error>> {
     let mut string = String::new();
     print!("{}", str);
     std::io::stdout().flush()?;
